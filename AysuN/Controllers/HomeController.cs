7d14f40b885b6c05648638e30e9e4b816bb954db
Ayxa217 <ayxanrm-bp217@code.edu/az>
2025-04-29 22:20:27 -0800

    MVCclassroom

---
+++ AysuN/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AysuN.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
